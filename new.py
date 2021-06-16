481bfb15ec90d20d01f8411152445f6e6c980d13
Mmjajjj188 <85689154+Mmjajjj188@users.noreply.github.com>
2021-06-16 17:45:40 +0300

    QWQW

---
+++ new.py
@@ -0,0 +1,10 @@
+import os
+os.system('pip install requests')
+os.system('pip install random')
+os.system('pip install colorama')
+os.system('pip install time')
+os.system('pip install dhooks')
+os.system('pip install requests')
+os.system('pip install user_agent')
+os.system('pip install colorama')
+os.system('pip install pyfiglet')
